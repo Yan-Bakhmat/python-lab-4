b18710b568e26303840e15f8fdf72965951fc65e
Yan-Bakhmat <yan.bakhmat@gmail.com>
2023-12-16 21:23:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,8 @@
-import mysql.connector
-from mysql.connector import Error  # error function separately
 import pandas as pd
+from dbfunc import *
 
-# function to connect to MySQL server
-
-
-def create_server_connection(host_name, user_name, user_password):
-    connection = None  # close any existing connection
-    try:  # try ... exept - handle any error and don't crush program
-        connection = mysql.connector.connect(
-            host=host_name,
-            user=user_name,
-            password=user_password
-        )
-        print('MySQL Database connection')
-    except Error as err:
-        print(f'Error: "{err}"')
-
-    return connection
-
-
-connection = create_server_connection('localhost', 'root', '')
+password = input('Please enter your server password: ')
+# the third parameter should be the password from your server
+connection = create_server_connection('localhost', 'root', password)
+create_datebase_query = "CREATE DATABASE dut"
+create_datebase(connection, create_datebase_query)
